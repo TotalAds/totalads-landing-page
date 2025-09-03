55381faba525d2f41289dbf6c0a7e8f77d198753
rehan-bprhub <rehan.qureshi@bprhub.com>
2025-09-03 20:15:35 +0530

    added g tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,6 +7,18 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-21HXVX47P3"
+        />
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-21HXVX47P3');`}
+        </script>
       </body>
     </Html>
   );
